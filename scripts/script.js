8d937cc26646960deb4b184598d5509c2afa9f05
Michael Pereira <michael.w.pereira.7@gmail.com>
2020-09-04 15:32:48 -0400

    Uploading website files

---
+++ scripts/script.js
@@ -0,0 +1,99 @@
+/*----------NavBar Tuning----------*/
+
+const navSlide = () => {
+	const burger = document.querySelector(".burger");
+	const nav = document.querySelector(".nav-links");
+	const navLinks = document.querySelectorAll(".nav-links li");
+
+	burger.addEventListener("click", () => {
+		nav.classList.toggle("nav-active");
+
+		navLinks.forEach((link, index) => {
+			if (link.style.animation) {
+				link.style.animation = "";
+			} else {
+				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+			}
+		});
+
+		burger.classList.toggle("toggle");
+	});
+};
+
+const navBackground = () => {
+	const nav = document.getElementsByTagName("nav")[0];
+
+	window.addEventListener("scroll", () => {
+		if (this.scrollY > 50) {
+			nav.classList.add("is-solid");
+			$("nav img").show();
+			} else {
+			nav.classList.remove("is-solid");
+			$("nav img").hide();
+		}
+	});
+};
+
+$(".nav-links li a").click(function () {
+	const nav = document.querySelector(".nav-links");
+	if (nav.classList.contains("nav-active")) {
+		document.getElementsByClassName("burger")[0].click();
+	}
+});
+
+navSlide();
+
+navBackground();
+
+/*----------Responsive Scroll----------*/
+
+let scrollMargin = 0;
+
+if (window.innerWidth > 1600) {
+	scrollMargin = -25;
+} else {
+	scrollMargin = window.innerHeight * 0.15;
+}
+
+$("#Home-Nav").addClass("active");
+
+$("#Home-Nav").click(function () {
+	$("html, body").animate(
+		{
+			scrollTop: $("#Home").offset().top,
+		},
+		1000
+	);
+	return false;
+});
+
+$("#About-Nav").click(function () {
+	$("html, body").animate(
+		{
+			scrollTop: $("#About").offset().top - scrollMargin,
+		},
+		1000
+	);
+	return false;
+});
+
+$("#Gallery-Nav").click(function () {
+	$(" html,body").animate(
+		{
+			scrollTop: $("#Gallery").offset().top - scrollMargin,
+		},
+		1000
+	);
+	return false;
+});
+
+$("#Contact-Nav").click(function () {
+	$(this).addClass("active");
+	$("html,body ").animate(
+		{
+			scrollTop: $("#Contact").offset().top - scrollMargin,
+		},
+		1000
+	);
+	return false;
+});
