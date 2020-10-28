24d0f2bf3841d5b9163b69e5c34deac9a0205621
Michael Pereira <michael.w.pereira.7@gmail.com>
2020-10-28 10:20:30 -0400

    Formatting code

--- scripts/script.js
+++ scripts/script.js
@@ -1,44 +1,43 @@
 /*----------NavBar Tuning----------*/
-
 const navSlide = () => {
-	const burger = document.querySelector(".burger");
-	const nav = document.querySelector(".nav-links");
-	const navLinks = document.querySelectorAll(".nav-links li");
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
 
-	burger.addEventListener("click", () => {
-		nav.classList.toggle("nav-active");
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("nav-active");
 
-		navLinks.forEach((link, index) => {
-			if (link.style.animation) {
-				link.style.animation = "";
-			} else {
-				link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-			}
-		});
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = "";
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
 
-		burger.classList.toggle("toggle");
-	});
+        burger.classList.toggle("toggle");
+    });
 };
 
 const navBackground = () => {
-	const nav = document.getElementsByTagName("nav")[0];
+    const nav = document.getElementsByTagName("nav")[0];
 
-	window.addEventListener("scroll", () => {
-		if (this.scrollY > 50) {
-			nav.classList.add("is-solid");
-			$("nav img").show();
-			} else {
-			nav.classList.remove("is-solid");
-			$("nav img").hide();
-		}
-	});
+    window.addEventListener("scroll", () => {
+        if (this.scrollY > 50) {
+            nav.classList.add("is-solid");
+            $("nav img").show();
+        } else {
+            nav.classList.remove("is-solid");
+            $("nav img").hide();
+        }
+    });
 };
 
 $(".nav-links li a").click(function () {
-	const nav = document.querySelector(".nav-links");
-	if (nav.classList.contains("nav-active")) {
-		document.getElementsByClassName("burger")[0].click();
-	}
+    const nav = document.querySelector(".nav-links");
+    if (nav.classList.contains("nav-active")) {
+        document.getElementsByClassName("burger")[0].click();
+    }
 });
 
 navSlide();
@@ -46,70 +45,62 @@ navSlide();
 navBackground();
 
 /*----------Responsive Scroll----------*/
-
 let scrollMargin = 0;
 
 if (window.innerWidth > 1600) {
-	scrollMargin = 10;
-}
-else if (window.innerWidth <= 900){
-	scrollMargin = 50;
-}
-else{
-	scrollMargin = 0;
+    scrollMargin = 10;
+} else if (window.innerWidth <= 900) {
+    scrollMargin = 50;
+} else {
+    scrollMargin = 0;
 }
 
-window.onresize = function(event) {
+window.onresize = function (event) {
     if (window.innerWidth > 1600) {
-		scrollMargin = 10;
-	}
-	else if (window.innerWidth <= 900){
-		scrollMargin = 50;
-	}
-	else{
-		scrollMargin = 0;
-	}
+        scrollMargin = 10;
+    } else if (window.innerWidth <= 900) {
+        scrollMargin = 50;
+    } else {
+        scrollMargin = 0;
+    }
 };
 
-
 $("#Home-Nav").click(function () {
-	$("html, body").animate(
-		{
-			scrollTop: $("#Home").offset().top,
-		},
-		1000
-	);
-	return false;
+    $("html, body").animate(
+        {
+            scrollTop: $("#Home").offset().top,
+        },
+        1000
+    );
+    return false;
 });
 
 $("#About-Nav").click(function () {
-	$("html, body").animate(
-		{
-			scrollTop: $("#About").offset().top - scrollMargin,
-		},
-		1000
-	);
-	return false;
+    $("html, body").animate(
+        {
+            scrollTop: $("#About").offset().top - scrollMargin,
+        },
+        1000
+    );
+    return false;
 });
 
 $("#Gallery-Nav").click(function () {
-	$(" html,body").animate(
-		{
-			scrollTop: $("#Gallery").offset().top - scrollMargin,
-		},
-		1000
-	);
-	return false;
+    $(" html,body").animate(
+        {
+            scrollTop: $("#Gallery").offset().top - scrollMargin,
+        },
+        1000
+    );
+    return false;
 });
 
 $("#Contact-Nav").click(function () {
-	$("html,body ").animate(
-		{
-			scrollTop: $("#Contact").offset().top - scrollMargin,
-		},
-		1000
-	);
-	return false;
+    $("html,body ").animate(
+        {
+            scrollTop: $("#Contact").offset().top - scrollMargin,
+        },
+        1000
+    );
+    return false;
 });
-
-		  
